630417814dac5c76dae92ebc629e6ddfaecad111
Ray Amac <june@opic.club>
2026-09-16 10:15:31 +0900

    삼청당 메인 페이지 Renewal Sample

---
+++ common.js
@@ -0,0 +1,113 @@
+/**
+ * 이미지 컨트롤
+ **/
+function imgPosition(img,imgSrc){
+	var imgs = new Image(); 
+	imgs.src = imgSrc.src;
+	var wid = 0;
+	var hei = 0;
+	document.getElementById(img).src 	= imgSrc.src;	
+}
+
+/**
+ * 기본정보, 상세보기 컨트롤
+ **/
+function viewProdDisplay(viewNum){
+	if(viewNum==1){
+		document.getElementById("basisDiv").style.display = "";
+		document.getElementById("basisImg").src = local+"common/images/common/table/view_gubun01.gif";
+		document.getElementById("infoDiv").style.display = "none";
+		document.getElementById("infoImg").src = local+"common/images/common/table/view_gubun02_out.gif";
+	}
+	else{
+		document.getElementById("basisDiv").style.display = "none";
+		document.getElementById("basisImg").src = local+"common/images/common/table/view_gubun01_out.gif";
+		document.getElementById("infoDiv").style.display = "";
+		document.getElementById("infoImg").src = local+"common/images/common/table/view_gubun02.gif";
+	}
+	
+}
+
+var backAddA = null;
+
+/*플래쉬에서 전달받기*/
+function flashLoad(returnVal){
+	if(backAddA){
+		backAddA.className = "";
+	}
+	var addA = document.getElementById("addA_"+returnVal);
+	if(addA){
+		addA.className = "sele01";
+		backAddA = addA;
+	}
+//	
+//	var flashHTML = "";
+//	flashHTML += '<object id="flashOJB" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="300" height="390">';
+//	flashHTML += '  <param name=movie value="../common/images/flash/trade.swf">';
+//	flashHTML += ' <param name=quality value=high>';
+//	flashHTML += ' <param name="FlashVars" value="main='+returnVal+'">';
+//	flashHTML += ' <embed src="../common/images/flash/trade.swf" quality=high pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="300" height="390">';
+//	flashHTML += ' </embed> ';
+//	flashHTML += '</object>';
+//	document.getElementById("flashDiv").innerHTML = flashHTML;
+}
+function flashLoadOut(returnVal){
+//	var addA1 = document.getElementById("addA_"+returnVal);
+//	if(addA1){
+//		addA1.className = "";
+//	}
+}
+/*플래쉬로 값 보내기*/
+function flashSendGo(returnVal){
+	//alert(returnVal);
+	//FlexSo.addParam("allowScriptAccess", "always");
+	//document.getElementById("flashDiv").innerHTML = returnVal;
+	//var flashOJB = document.getElementById("flashOJB");
+	//flashOJB.addParam("FlashVars", "main=7"); 
+	var flashHTML = "";
+	flashHTML += '<object id="flashOJB" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="300" height="390">';
+	flashHTML += '  <param name=movie value="../common/images/flash/trade.swf">';
+	flashHTML += ' <param name=quality value=high>';
+	flashHTML += ' <param name="FlashVars" value="main='+returnVal+'">';
+	flashHTML += ' <embed src="../common/images/flash/trade.swf" quality=high pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="300" height="390">';
+	flashHTML += ' </embed> ';
+	flashHTML += '</object>';
+	document.getElementById("flashDiv").innerHTML = flashHTML;
+	
+	//document.getElementById("flashOJB").SetVariable("main", "7");
+}
+
+function smsOpen(vUrl){
+	var vWinName = "sms";
+	var vOption = "scrollbars=yes,width=430,height=542";
+	widOpen(vUrl,vWinName,vOption);
+}
+
+function emailOpen(vUrl){
+	var vWinName = "email보내기";
+	var vOption = "scrollbars=yes,width=700,height=685";
+	widOpen(vUrl,vWinName,vOption); 
+}
+
+/* 윈도우 오픈 */
+function widOpen(theURL,winName,features){
+	window.open(theURL,winName,features); 
+}
+
+/* 윈도우 닫기 */
+function closePopupWin(){
+	window.close();
+}
+
+/* 약국찾기 검색조건 변경시 호출 */
+function searchCondition_Change(obj){
+	if(obj.value=="1"){
+		document.getElementById("searchTb1").style.display = "";
+		document.getElementById("searchTb2").style.display = "none";
+		document.getElementById("searchCondition1").value = obj.value;
+	}else{
+		document.getElementById("searchTb1").style.display = "none";
+		document.getElementById("searchTb2").style.display = "";
+		document.getElementById("searchCondition2").value = obj.value;
+	}
+}
---
+++ menuXmlControl.js
@@ -0,0 +1,157 @@
+var basePath = null;
+var oXML  = new ActiveXObject("MSXML.DOMDocument");
+oXML.async  = false;
+if( oXML.load( local+"getMenuMapping.xml" ) ){
+ oXML.setProperty("SelectionLanguage","XPath");
+}
+
+function initialize(menuNum,subMenuNum){
+	if(menuNum!=undefined){
+	 leftMenuView(menuNum,subMenuNum);
+	}
+}
+
+function overImg(obj, img, menuNum){
+	var imgUrl = local+"common/images/common/menu/"+img+"_over.gif";
+	obj.src = imgUrl;
+	topSubMenuView(menuNum);
+	showDetails('topSubMenuDiv');
+}
+
+function outImg(obj, img){
+	var imgUrl = local+"common/images/common/menu/"+img+".gif";
+	obj.src = imgUrl;
+	startTimer(obj);
+	//document.getElementById("topSubMenuDiv").style.display = "none";
+}
+
+//<!--전반적인 png파일 표시--> 
+function setPng24(obj) {
+    obj.width=obj.height=1;
+    obj.className=obj.className.replace(/\bpng24\b/i,'');
+    obj.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='"+obj.src+"',sizingMethod='image');"
+    obj.src='';
+    return '';
+}
+
+//상단 메뉴의 서브메뉴 리스트 보이기
+function topSubMenuView(num){
+	var prodList= oXML.selectNodes("/Menu/Group");
+	
+	if(prodList[num]){
+		var leftMargin = prodList[num].getAttribute("leftMargin");
+		var menuHTML = "<table cellpadding='0' cellspacing='0' border='0' align='left'><tr>";
+		var widSize = 0;
+		var subList = prodList[num].selectNodes("Item");
+		for(var i=0; i<subList.length;i++){
+			widSize += eval(subList[i].getAttribute("menuWidth"));
+			menuHTML += "<td width='"+subList[i].getAttribute("menuWidth")+"px'>";
+			menuHTML += "<a href='"+local+subList[i].getAttribute("loadFile")+"'>"+subList[i].getAttribute("name")+"</a>";
+			menuHTML += "</td>";
+		}
+		menuHTML += "</tr></table>";
+		document.getElementById("topSubMenuTxtDiv").innerHTML = menuHTML;
+		document.getElementById("topSubMenuDiv").style.display = "";
+		document.getElementById("subMenuBg").style.width= widSize+"px";
+		
+		var menuWid = FindPosX(document.getElementById("topMenuTB"))+eval(leftMargin);
+		document.getElementById("topSubMenuDiv").style.left= menuWid;
+	}else{
+		document.getElementById("topSubMenuDiv").style.display = "none";
+	}
+}
+
+//왼쪽메뉴 뷰
+function leftMenuView(num, subNum){
+	var prodList= oXML.selectNodes("/Menu/Group");
+	if(prodList[num]){
+		var menuHTML = "<table cellpadding='0' cellspacing='0' border='0' class='leftMenuTxtTB2'>";
+		var tdObjVal = " ";
+		var className = " ";
+		var subList = prodList[num].selectNodes("Item");
+		for(var i=0; i<subList.length ;i++){
+			tdObjVal = ' onmouseover="leftDepthOver(this)" onmouseout="leftDepthOut(this)"';
+			className = " class='selectedDepth01'";
+			if(i==subNum){
+				tdObjVal = ' onmouseover="leftDepthOver(this)" onmouseout="leftDepthOut(this,\'depth\')"';
+				className = " class='selectedDepth02'";
+			}
+			menuHTML += "<tr><td "+className+tdObjVal+" onclick='leftDepthClick(\""+local+subList[i].getAttribute("loadFile")+"\")'>";
+			menuHTML += "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+subList[i].getAttribute("name");
+			menuHTML += "</td></tr>";
+		}
+		menuHTML += "</table>";
+		document.getElementById("depth01List").innerHTML = menuHTML;
+	}
+}
+
+function leftDepthOver(obj){
+	var classNameVal = "selectedDepth03";
+	obj.className = classNameVal;
+}
+
+function leftDepthOut(obj, depth){
+	var classNameVal = "selectedDepth01";
+	if(depth){
+		classNameVal = "selectedDepth02";
+	}
+	obj.className = classNameVal;
+}
+
+function leftDepthClick(locationUrl){
+	document.location.href = locationUrl
+}
+
+function leftDepth(obj, depth){
+	var classNameVal = "selectedDepth01";
+	if(depth){
+		classNameVal = "selectedDepth02";
+	}
+	obj.className = classNameVal;
+}
+
+/*
+function leftMenuView(num, subNum){
+	var prodList= oXML.selectNodes("/Menu/Group");
+	if(prodList[num]){
+		var menuHTML = "<ul class='localDepth01List'><li>";
+		var className = " ";
+		var subList = prodList[num].selectNodes("Item");
+		for(var i=0; i<subList.length ;i++){
+			menuHTML += "<ul>";
+			className = " ";
+			if(i==subNum){className = "class='selectedDepth02'";}
+			menuHTML += "<a href='"+local+subList[i].getAttribute("loadFile")+"' "+className+">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+subList[i].getAttribute("name")+"</a>";
+			menuHTML += "</ul>";
+		}
+		menuHTML += "</li></ul>";
+		document.getElementById("depth01List").innerHTML = menuHTML;
+	}
+}*/
+
+function FindPosX(obj) {
+	var fObj = obj;
+	var curleft = 0;
+	if (obj.offsetParent) {
+		while (obj.offsetParent) {
+			curleft += (obj.offsetLeft - obj.scrollLeft);
+			obj = obj.offsetParent;
+		}
+	} else if (obj.x)
+		curleft += obj.x;
+	return curleft;
+}
+
+function FindPosY(obj) {
+	var curtop = 0;
+	if (obj.offsetParent) {
+		while (obj.offsetParent) {
+			curtop += (obj.offsetTop - obj.scrollTop);
+			obj = obj.offsetParent;
+		}
+	} else if (obj.y)
+		curtop += obj.y;
+
+	return curtop;
+}
+
